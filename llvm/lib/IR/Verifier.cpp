22ca8d47e3e8db266f2d3e1b95763531e0d355f5
usuyus <yusufonurusumez@gmail.com>
2025-08-15 11:01:32 +0100

    CCName

--- llvm/lib/IR/Verifier.cpp
+++ llvm/lib/IR/Verifier.cpp
@@ -3583,7 +3583,8 @@ void Verifier::verifyMustTailCall(CallInst &CI) {
       CI.getCallingConv() == CallingConv::Tail ||
       CI.getCallingConv() == CallingConv::OCaml) {
     StringRef CCName =
-        CI.getCallingConv() == CallingConv::Tail ? "tailcc" : "swifttailcc";
+        CI.getCallingConv() == CallingConv::Tail ? "tailcc" :
+        CI.getCallingConv() == CallingConv::SwiftTail ? "swifttailcc" : "ocamlcc";
 
     // - Only sret, byval, swiftself, and swiftasync ABI-impacting attributes
     //   are allowed in swifttailcc call
