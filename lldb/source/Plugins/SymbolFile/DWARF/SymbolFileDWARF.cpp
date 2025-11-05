1c6fe5467f7a55cd87e6a4659a264b3dc94163c7
Tim McGilchrist <timmcgil@gmail.com>
2025-11-05 00:02:44 +0000

    fixup! Add OxCaml runlength demangling

--- lldb/source/Plugins/SymbolFile/DWARF/SymbolFileDWARF.cpp
+++ lldb/source/Plugins/SymbolFile/DWARF/SymbolFileDWARF.cpp
@@ -1338,7 +1338,17 @@ size_t SymbolFileDWARF::ParseBlocksRecursive(
                 comp_unit.GetSupportFiles().GetFileSpecAtIndex(call_file),
                 call_line, call_column);
 
-          block->SetInlinedFunctionInfo(name, mangled_name, decl_up.get(),
+          // If mangled_name is NULL but name is mangled (e.g., OxCaml symbols),
+          // swap them so the Mangled class can handle demangling correctly
+          const char *use_name = name;
+          const char *use_mangled = mangled_name;
+          if (mangled_name == nullptr && name != nullptr &&
+              Mangled::GetManglingScheme(name) != Mangled::eManglingSchemeNone) {
+            use_mangled = name;
+            use_name = nullptr;
+          }
+
+          block->SetInlinedFunctionInfo(use_name, use_mangled, decl_up.get(),
                                         call_up.get());
         }
 
